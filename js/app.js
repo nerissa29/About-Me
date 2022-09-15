f1285e63fc17c5f186740b47e8b5fffde004f310
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-09-15 03:03:18 +0000

    update

--- js/app.js
+++ js/app.js
@@ -2,14 +2,17 @@
 
 console.log('Hi! Welcome to my page!');
 
+
 let userName = prompt('What is your name?');
 
 alert(`Welcome to my site ${userName}!`);
 
+let score = 0;
 // let question1 = prompt('Do I have kids?').toLowerCase();
 
 // if (question1 === 'y' || question1 === 'yes') {
 //   // console.log(`${userName}'s guess is correct!`);
+//   score++;
 //   alert(`${userName}, you are correct. I have two kids!`);
 // } else if (question1 === 'n' || question1 === 'no') {
 //   // console.log(`${userName}'s guess is wrong!`);
@@ -23,6 +26,7 @@ alert(`Welcome to my site ${userName}!`);
 //   alert(`${userName}, you are wrong! My pet's name is Raiss!`);
 // } else if (question2 === 'n' || question2 === 'no') {
 //   // console.log(`${userName}'s guess is correct!`);
+//   score++;
 //   alert(`${userName}, you are correct! I don't have a pet name Petra!`);
 // }
 
@@ -30,6 +34,7 @@ alert(`Welcome to my site ${userName}!`);
 
 // if (question3 === 'y' || question3 === 'yes') {
 //   // console.log(`${userName}'s guess is correct!`);
+//   score++;
 //   alert(`${userName}, you are correct! I served 8 years Active Duty in the Navy!`);
 // } else if (question3 === 'n' || question3 === 'no') {
 //   // console.log(`${userName}'s guess is wrong!`);
@@ -43,6 +48,7 @@ alert(`Welcome to my site ${userName}!`);
 //   alert(`${userName}, you are wrong! I am a Logistic Specialist working in Supply department!`);
 // } else if (question4 === 'n' || question4 === 'no') {
 //   // console.log(`${userName}'s guess is correct!`);
+//   score++;
 //   alert(`${userName}, you are correct! I work in supply department as a Logistic Specialist!`);
 // }
 
@@ -50,6 +56,7 @@ alert(`Welcome to my site ${userName}!`);
 
 // if (question5 === 'y' || question5 === 'yes') {
 //   // console.log(`${userName}'s guess is correct!`);
+//   score++;
 //   alert(`${userName}, you are correct! My age is no longer in the calendar!`);
 // } else if (question5 === 'n' || question5 === 'no') {
 //   // console.log(`${userName}'s guess is wrong!`);
@@ -60,23 +67,48 @@ alert(`Welcome to my site ${userName}!`);
 let question6 = Number(prompt('Guess a number from 1 - 20!'));
 
 let num = 8;
-let count = 1;
+let count = 0;
 
-while (count < 4) {
+
+// while (count < 4) {
+for (let i = count; i < 4; i++) {
   if (question6 === num) {
+    score++;
     alert('You are CORRECT!');
+    // i = 4;
     break;
-  } else if (question6 < num) alert('Too low!');
-  else if (question6 > num) alert('Too high!');
+  } else if (question6 < num) {
+    alert('Too low!');
+    count++;
+  } else if (question6 > num) {
+    alert('Too high!');
+    count++;
+  }
 
   question6 = Number(prompt('Guess a number from 1 - 20!'));
-  count++;
 }
 
-alert(`${userName}, the correct number is ${num}`);
+alert(`The number I have is ${num}`);
 
-let arr = ['black', 'cyan', 'purple', 'w'];
 
-// let question7 = prompt()
+let arr = ['black', 'cyan', 'purple', 'magenta', 'fuchsia', 'lime', 'teal', 'navy', 'olive'];
 
-alert(`Thank you ${userName} for playing this game with me. Have a great day!`);
+let question7 = prompt(`${userName}, you only have 6 tries! Try to guess a color!`).toLowerCase();
+
+
+let guess = false;
+
+
+for (let i = 0; i <= 6; i++) {
+  for (let j = 0; j < arr.length; j++) {
+    if (question7 === arr[j]) {
+      score++;
+      alert(`Congrats ${userName}! You guessed it RIGHT!`);
+      guess = true;
+    }
+  }
+  if (guess) break;
+  question7 = prompt(`Try agan! ${userName}, try to guess a color!`);
+}
+
+alert(`Thank you ${userName} for playing this game with me. You got ${score} correct answers! Wooohoo! Have a great day!`);
