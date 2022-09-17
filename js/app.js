05402ddb1449422ea06ef896dddf15a82a7816bf
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-09-17 22:47:22 +0000

    uncomment questions 1-5

--- js/app.js
+++ js/app.js
@@ -2,94 +2,91 @@
 
 console.log('Hi! Welcome to my page!');
 
-
 let userName = prompt('What is your name?');
 
 alert(`Welcome to my site ${userName}!`);
 
 let score = 0;
-// function questionOne(){
-//   let question1 = prompt('Do I have kids?').toLowerCase();
+function questionOne(){
+  let question1 = prompt('Do I have kids?').toLowerCase();
 
-//   if (question1 === 'y' || question1 === 'yes') {
-//     // console.log(`${userName}'s guess is correct!`);
-//     score++;
-//     alert(`${userName}, you are correct. I have two kids!`);
-//   } else if (question1 === 'n' || question1 === 'no') {
-//     // console.log(`${userName}'s guess is wrong!`);
-//     alert(`${userName}, you are wrong! I am a mother of two!`);
-//   }
+  if (question1 === 'y' || question1 === 'yes') {
+    // console.log(`${userName}'s guess is correct!`);
+    score++;
+    alert(`${userName}, you are correct. I have two kids!`);
+  } else if (question1 === 'n' || question1 === 'no') {
+    // console.log(`${userName}'s guess is wrong!`);
+    alert(`${userName}, you are wrong! I am a mother of two!`);
+  }
 
-// }
-// questionOne();
+}
+questionOne();
 
 
-// function questionTwo(){
-//   let question2 = prompt('Do I have a pet named Petra?').toLowerCase();
+function questionTwo(){
+  let question2 = prompt('Do I have a pet named Petra?').toLowerCase();
 
-//   if (question2 === 'y' || question2 === 'yes') {
-//     // console.log(`${userName}'s guess is wrong!`);
-//     alert(`${userName}, you are wrong! My pet's name is Raiss!`);
-//   } else if (question2 === 'n' || question2 === 'no') {
-//     // console.log(`${userName}'s guess is correct!`);
-//     score++;
-//     alert(`${userName}, you are correct! I don't have a pet name Petra!`);
-//   }
+  if (question2 === 'y' || question2 === 'yes') {
+    // console.log(`${userName}'s guess is wrong!`);
+    alert(`${userName}, you are wrong! My pet's name is Raiss!`);
+  } else if (question2 === 'n' || question2 === 'no') {
+    // console.log(`${userName}'s guess is correct!`);
+    score++;
+    alert(`${userName}, you are correct! I don't have a pet name Petra!`);
+  }
 
-// }
-// questionTwo();
+}
+questionTwo();
 
-// function questionThree(){
-//   let question3 = prompt('Did I served in the military?').toLowerCase();
+function questionThree(){
+  let question3 = prompt('Did I served in the military?').toLowerCase();
 
-//   if (question3 === 'y' || question3 === 'yes') {
-//     // console.log(`${userName}'s guess is correct!`);
-//     score++;
-//     alert(`${userName}, you are correct! I served 8 years Active Duty in the Navy!`);
-//   } else if (question3 === 'n' || question3 === 'no') {
-//     // console.log(`${userName}'s guess is wrong!`);
-//     alert(`${userName}, you are wrong! I was in ACtive Duty for 8 years!`);
-//   }
-// }
+  if (question3 === 'y' || question3 === 'yes') {
+    // console.log(`${userName}'s guess is correct!`);
+    score++;
+    alert(`${userName}, you are correct! I served 8 years Active Duty in the Navy!`);
+  } else if (question3 === 'n' || question3 === 'no') {
+    // console.log(`${userName}'s guess is wrong!`);
+    alert(`${userName}, you are wrong! I was in ACtive Duty for 8 years!`);
+  }
+}
 
-// questionThree();
+questionThree();
 
-// function questionFour(){
-//   let question4 = prompt('Did I work as a Yeoman in admin department?').toLowerCase();
+function questionFour(){
+  let question4 = prompt('Did I work as a Yeoman in admin department?').toLowerCase();
 
-//   if (question4 === 'y' || question4 === 'yes') {
-//     // console.log(`${userName}'s guess is wrong!`);
-//     alert(`${userName}, you are wrong! I am a Logistic Specialist working in Supply department!`);
-//   } else if (question4 === 'n' || question4 === 'no') {
-//     // console.log(`${userName}'s guess is correct!`);
-//     score++;
-//     alert(`${userName}, you are correct! I work in supply department as a Logistic Specialist!`);
-//   }
-// }
+  if (question4 === 'y' || question4 === 'yes') {
+    // console.log(`${userName}'s guess is wrong!`);
+    alert(`${userName}, you are wrong! I am a Logistic Specialist working in Supply department!`);
+  } else if (question4 === 'n' || question4 === 'no') {
+    // console.log(`${userName}'s guess is correct!`);
+    score++;
+    alert(`${userName}, you are correct! I work in supply department as a Logistic Specialist!`);
+  }
+}
 
-// questionFour();
+questionFour();
 
-// function questionFive(){
-//   let question5 = prompt('Am I on my 30\'s?').toLowerCase();
+function questionFive(){
+  let question5 = prompt('Am I on my 30\'s?').toLowerCase();
 
-//   if (question5 === 'y' || question5 === 'yes') {
-//     // console.log(`${userName}'s guess is correct!`);
-//     score++;
-//     alert(`${userName}, you are correct! My age is no longer in the calendar!`);
-//   } else if (question5 === 'n' || question5 === 'no') {
-//     // console.log(`${userName}'s guess is wrong!`);
-//     alert(`${userName}, as much as I want you to be correct, you are wrong!`);
+  if (question5 === 'y' || question5 === 'yes') {
+    // console.log(`${userName}'s guess is correct!`);
+    score++;
+    alert(`${userName}, you are correct! My age is no longer in the calendar!`);
+  } else if (question5 === 'n' || question5 === 'no') {
+    // console.log(`${userName}'s guess is wrong!`);
+    alert(`${userName}, as much as I want you to be correct, you are wrong!`);
 
-//   }
-// }
+  }
+}
 
-// questionFive();
+questionFive();
 
 
 // Question 6 added for Lab 03
 let num = 8;
-
-
 
 for (let i = 0; i < 4; i++) {
   // let count = 0;
@@ -128,7 +125,6 @@ for (let i = 0; i < 6; i++) {
     }
   }
   if (guess) break;
-  // question7 = prompt(`Try agan! ${userName}, try to guess a color!`);
 }
 
 alert(`The colors I have picked are ${arr.join(', ')}`);
@@ -139,7 +135,7 @@ alert(`Thank you ${userName} for playing this game with me. You got ${score} cor
 
 /*
 
-This part is for Lab04
+This part is for Lab04, still working on it and will be submitting soon
 
 // question 6
 let num = 8;
